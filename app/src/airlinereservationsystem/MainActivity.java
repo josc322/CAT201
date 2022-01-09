49a882cb47d49f0e8cfc58fce52677339c7566de
josc322 <96969953+josc322@users.noreply.github.com>
2022-01-09 19:44:11 +0800

    Create MainActivity.java

---
+++ app/src/airlinereservationsystem/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.airlinereservationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner destinationFrom = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> destinationFromAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.destinationFromNames));
+        destinationFromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destinationFrom.setAdapter(destinationFromAdapter);
+
+        Spinner destinationTo = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> destinationToAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.destinationToNames));
+        destinationToAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destinationTo.setAdapter(destinationToAdapter);
+    }
+}
