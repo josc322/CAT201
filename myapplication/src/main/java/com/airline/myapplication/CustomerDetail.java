24616644d58c94e1931ba782290fe7911901d57e
deftmemeda <96969953+josc322@users.noreply.github.com>
2022-01-30 10:56:17 +0800

    Update CustomerDetail.java

--- myapplication/src/main/java/com/airline/myapplication/CustomerDetail.java
+++ myapplication/src/main/java/com/airline/myapplication/CustomerDetail.java
@@ -1,20 +1,20 @@
 package com.airline.myapplication;
 
 public class CustomerDetail {
-    public String cus_email;
-    public String cus_phone;
-    public String cus_name;
-    public String cus_age;
+    public String customerEmail;
+    public String customerPhone;
+    public String customerName;
+    public String customerAge;
 
-
-    public CustomerDetail(String cus_email, String cus_phone, String cus_name,String cus_age) {
-        this.cus_email = cus_email;
-        this.cus_phone = cus_phone;
-        this.cus_name=cus_name;
-        this.cus_age = cus_age;
-    }
-
+    //Default constructor
     public CustomerDetail(){
 
+    }
+
+    public CustomerDetail(String email, String phone, String name,String age) {
+        this.customerEmail = email;
+        this.customerPhone = phone;
+        this.customerName = name;
+        this.customerAge = age;
     }
 }
