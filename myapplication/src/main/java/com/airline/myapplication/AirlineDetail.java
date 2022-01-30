7a11581f2ac503af0e144b88c86423fed2ea4455
deftmemeda <96969953+josc322@users.noreply.github.com>
2022-01-30 11:17:07 +0800

    Update AirlineDetail.java

--- myapplication/src/main/java/com/airline/myapplication/AirlineDetail.java
+++ myapplication/src/main/java/com/airline/myapplication/AirlineDetail.java
@@ -7,20 +7,16 @@ public class AirlineDetail{
     public String date;
     public String from_airline;
     public String to_airline;
-    public String time_reach;
-    public String time_from;
 
-    public AirlineDetail(String airlineId, String airlineName, String cabinClass, String date, String from, String to, String time_from, String time_reach) {
+    public AirlineDetail() {
+    }
+    
+    public AirlineDetail(String airlineId, String airlineName, String cabinClass, String date, String from, String to) {
         this.airlineId = airlineId;
         this.airlineName = airlineName;
         this.cabinClass = cabinClass;
         this.date = date;
         this.from_airline = from;
         this.to_airline = to;
-        this.time_from = time_from;
-        this.time_reach = time_reach;
-    }
-
-    public AirlineDetail() {
     }
 }
